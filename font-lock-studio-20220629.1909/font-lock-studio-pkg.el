651e84c2da93da1aad1fd3beddc11faf90a08b72
Benson Chu <bensonchu457@gmail.com>
2022-10-22 10:08:58 -0500

    Adding font-lock-studio

---
+++ font-lock-studio-20220629.1909/font-lock-studio-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from font-lock-studio.el  -*- no-byte-compile: t -*-
+(define-package "font-lock-studio" "20220629.1909" "interactive debugger for Font Lock keywords." '((emacs "24.3")) :commit "78472ae1f65721b4da17756ee7e506f3d0487033" :authors '(("Anders Lindgren")) :maintainer '("Anders Lindgren") :keywords '("faces" "tools") :url "https://github.com/Lindydancer/font-lock-studio")
