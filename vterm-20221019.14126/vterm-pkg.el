d64956f80f30499354b8ef833e8a65a7fdb494fc
Benson Chu <bensonchu457@gmail.com>
2022-10-28 16:24:30 -0500

    Adding vterm

---
+++ vterm-20221019.14126/vterm-pkg.el
@@ -0,0 +1,12 @@
+(define-package "vterm" "20221019.14126" "Fully-featured terminal emulator"
+  '((emacs "25.1"))
+  :authors
+  '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
+  :maintainer
+  '("Lukas Fürmetz" . "fuermetz@mailbox.org")
+  :keywords
+  '("terminals")
+  :url "https://github.com/akermu/emacs-libvterm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
