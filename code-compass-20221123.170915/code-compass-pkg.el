12404b743b4f301fd6275c2854f9faab1b54dcdf
Benson Chu <bensonchu457@gmail.com>
2022-11-25 08:20:08 -0600

    Use submodule instead

--- code-compass-20221123.170915/code-compass-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from code-compass.el  -*- no-byte-compile: t -*-
-(define-package "code-compass" "20221123.170915" "Make Emacs your compass in a sea of software complexity." 'nil :authors '(("Andrea Giugliano" . "agiugliano@live.it")) :maintainer '("Andrea Giugliano" . "agiugliano@live.it") :keywords '("emacs" "sofware" "analysis"))
