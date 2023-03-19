3e70c8299c0a9107c0b929bd22eb3c91aded3605
Benson Chu <bensonchu457@gmail.com>
2023-03-18 21:12:50 -0500

    getting back into visualization

---
+++ pair-tree-20211219.1816/pair-tree-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from pair-tree.el  -*- no-byte-compile: t -*-
+(define-package "pair-tree" "20211219.1816" "Visualize a list" '((emacs "27.1") (dash "2.17.0")) :commit "00bdaf9df933aaacbed66b5d666e2abc29870103" :authors '(("Zainab Ali <zainab @kebab-ca.se>")) :maintainer '("Zainab Ali <zainab @kebab-ca.se>") :keywords '("lisp" "tools") :url "https://github.com/zainab-ali/pair-tree")
