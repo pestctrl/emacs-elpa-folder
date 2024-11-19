81ffc031f8d7728ee0ee169a7947f86451b5e80d
Benson Chu <b-chu1@ti.com>
2024-11-19 15:41:03 -0600

    Upgraded realgud-lldb

---
+++ realgud-lldb-20241119.209/realgud-lldb-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "realgud-lldb" "20241119.209"
+  "Realgud front-end to lldb."
+  '((load-relative "1.3.1")
+    (realgud       "1.5.0")
+    (emacs         "25"))
+  :url "https://github.com/realgud/realgud-lldb"
+  :commit "deacd070e8ab8830f4d577fee37136ad89183d13"
+  :revdesc "deacd070e8ab"
+  :authors '(("Rocky Bernstein" . "rocky@gnu.org"))
+  :maintainers '(("Rocky Bernstein" . "rocky@gnu.org")))
