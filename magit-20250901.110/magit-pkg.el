6df853a359ff19a0a8fa40cc1dd52c0768a49129
Benson Chu <dev@mail.pestctrl.io>
2025-09-01 10:24:44 -0500

    Why can't magit just stay put?

---
+++ magit-20250901.110/magit-pkg.el
@@ -0,0 +1,19 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit" "20250901.110"
+  "A Git porcelain inside Emacs."
+  '((emacs         "28.1")
+    (compat        "30.1")
+    (cond-let      "0.1")
+    (llama         "1.0")
+    (magit-section "4.3.8")
+    (seq           "2.24")
+    (transient     "0.9.3")
+    (with-editor   "3.4.4"))
+  :url "https://github.com/magit/magit"
+  :commit "679736e111c2809f6f2e803bf29931078aee417e"
+  :revdesc "679736e111c2"
+  :keywords '("git" "tools" "vc")
+  :authors '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+             ("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
+                 ("Kyle Meyer" . "kyle@kyleam.com")))
